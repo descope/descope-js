a997e9d37e4c75563ebf1fd7c92ee2c215022378
Nir Gur Arie <nir@descope.com>
2023-03-07 15:58:08 +0200

    fix: test DUMMY

--- tools/scripts/latestTag.sh
+++ tools/scripts/latestTag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 output=$(git ls-remote --tags --sort="committerdate" origin 2> /dev/null | tail -n 1 | sed 's~.*refs/tags/~~' | sed 's/\\^{}//');
-if [[ -n $output ]];
+if [ -n $output ];
 then echo $output;
 else echo origin/$(git rev-parse --abbrev-ref HEAD);
 fi;
