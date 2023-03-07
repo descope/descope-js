eba7d66c768ddadbe740bc8c0d67c0c3b147badb
Asaf Shen <asaf@descope.com>
2023-03-07 11:03:36 +0200

    fix: btter tests

--- packages/core2/src/lib/core2.spec.ts
+++ packages/core2/src/lib/core2.spec.ts
@@ -2,6 +2,7 @@ import { core2 } from './core2';
 
 describe('core2', () => {
   it('should work', () => {
-    expect(core2()).toContain('core');
+    expect(core2()).toContain('core2_');
+    expect(core2()).toContain('core_');
   });
 });
