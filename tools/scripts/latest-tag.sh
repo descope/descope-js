e721ffb2d83d7918b1e3fbd49662d8f28011018d
Nir Gur Arie <nir@descope.com>
2023-03-07 15:54:18 +0200

    chore: script

--- tools/scripts/latest-tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-output=$(git ls-remote --tags --sort="committerdate" origin | tail -n 1 | sed 's~.*refs/tags/~~' | sed 's/\\^{}//')
-
-if [[ "$output" -gt 0 ]]; then
-  echo $output
-else
-  echo 'origin/nx'
-fi;
