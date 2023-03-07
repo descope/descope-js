28c48f5ec4d66f933a7ba18414fd3f2c6eca23fa
Nir Gur Arie <nir@descope.com>
2023-03-07 16:12:12 +0200

    chore: bash DUMMY

--- tools/scripts/latestTag.sh
+++ tools/scripts/latestTag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-output=$(git ls-remote --tags --sort="committerdate" origin 2> /dev/null | tail -n 1 | sed 's~.*refs/tags/~~' | sed 's/\\^{}//');
-if [ -n $output ];
-then echo $output;
-else echo origin/$(git rev-parse --abbrev-ref HEAD);
+output=$(git ls-remote --tags --sort="committerdate" origin);
+if [ -z $output ];
+then echo origin/$(git rev-parse --abbrev-ref HEAD);
+else echo $output;
 fi;
