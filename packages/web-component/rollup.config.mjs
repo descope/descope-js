25bc8e552fad38a435c12e9f8e4da2094095126b
Nir Gur Arie <nir@descope.com>
2023-03-20 12:52:55 +0200

    fix: wc

--- packages/web-component/rollup.config.mjs
+++ packages/web-component/rollup.config.mjs
@@ -6,7 +6,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import define from 'rollup-plugin-define';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 
 const input = 'src/lib/descope-wc/index.ts';
 
