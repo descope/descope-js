44248e3ca7d5f4aaf1dc50e7f369d03a98a55d73
GuyP <105154237+guyp-descope@users.noreply.github.com>
2023-05-10 15:24:16 +0300

    feat: add oidc flow start param (#120)
    ## Description
    
    Add the ability to pass OIDC url param ("stateId") into the flow.
    
    ## Must
    
    - [X] Tests
    - [X] Documentation (if applicable)

--- packages/web-component/src/lib/descope-wc/BaseDescopeWc.ts
+++ packages/web-component/src/lib/descope-wc/BaseDescopeWc.ts
@@ -394,6 +394,7 @@ class BaseDescopeWc extends HTMLElement {
         exchangeError,
         redirectAuthCallbackUrl,
         redirectAuthCodeChallenge,
+        oidcIdpStateId,
       } = handleUrlParams();
 
       // we want to update the state when user clicks on back in the browser
@@ -416,6 +417,7 @@ class BaseDescopeWc extends HTMLElement {
         telemetryKey: this.telemetryKey,
         redirectAuthCallbackUrl,
         redirectAuthCodeChallenge,
+        oidcIdpStateId,
       });
 
       this.#debugState.update({ isDebug: this.debug });
