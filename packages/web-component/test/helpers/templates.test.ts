eb98f421af1823d59e5c6872a4c4427bd81c4165
nirgur <nirgur@users.noreply.github.com>
2023-08-29 11:04:33 +0300

    fix tests (#258)
    fixes: https://github.com/descope/etc/issues/3900

--- packages/web-component/test/helpers/templates.test.ts
+++ packages/web-component/test/helpers/templates.test.ts
@@ -12,26 +12,22 @@ describe('templates', () => {
   it('should handle descope inputs', async () => {
     document.body.innerHTML = `<div>
 			<input class="descope-input" name="email">
-			<textarea class="descope-input" name="description">
 		</div>`;
 
     replaceWithScreenState(document, {
       inputs: { email: 'email1', description: 'description1' },
     });
     await waitFor(() => screen.getByShadowDisplayValue('email1'));
-    await waitFor(() => screen.getByShadowDisplayValue('description1'));
   });
 
   it('should handle descope form', async () => {
     document.body.innerHTML = `<div>
-			<input class="descope-input" name="email">
-			<textarea class="descope-input" name="description">
+			<input class="descope-input" name="email"></input>
 		</div>`;
 
     replaceWithScreenState(document, {
-      form: { email: 'email2', description: 'description2' },
+      form: { email: 'email2' },
     });
     await waitFor(() => screen.getByShadowDisplayValue('email2'));
-    await waitFor(() => screen.getByShadowDisplayValue('description2'));
   });
 });
