46ff54ca5bb9489944329b22be0594d6401f552d
Gil Shapira <gshapira@gmail.com>
2023-09-28 00:32:09 +0300

    Remove prefer-biometrics feature

--- packages/web-component/src/lib/descope-wc/DescopeWc.ts
+++ packages/web-component/src/lib/descope-wc/DescopeWc.ts
@@ -14,7 +14,6 @@ import {
   getInputValueByType,
   handleAutoFocus,
   injectSamlIdpForm,
-  isChromium,
   isConditionalLoginSupported,
   replaceWithScreenState,
   setTOTPVariable,
@@ -316,32 +315,6 @@ class DescopeWc extends BaseDescopeWc {
         return;
       }
 
-      // we override the default webauthn options to only use platform authenticators (i.e., builtin biometrics)
-      // when registering a new webauthn credential if we're running on Chrome and the device actually has such
-      // an authenticator. this makes it so in Chrome when the passkeys dialog pops up the user is immediately
-      // offered to use biometrics, rather than having to select it from several options. this behavior is enabled
-      // by default but can be disabled on the web-component itsel by setting prefer-biometrics="false".
-      let options = webauthnOptions;
-      if (
-        this.preferBiometrics &&
-        action === RESPONSE_ACTIONS.webauthnCreate &&
-        isChromium() &&
-        (await window.PublicKeyCredential?.isUserVerifyingPlatformAuthenticatorAvailable?.())
-      ) {
-        try {
-          const json = JSON.parse(options);
-          if (json.publicKey) {
-            json.publicKey.authenticatorSelection ||= {};
-            json.publicKey.authenticatorSelection.authenticatorAttachment ||=
-              'platform';
-            options = JSON.stringify(json);
-          }
-        } catch (e) {
-          // if options could not be parsed we ignore it here so this kind of error is always handled the same way
-          this.loggerWrapper.info('Failed to modify webauthn create options');
-        }
-      }
-
       this.#conditionalUiAbortController?.abort();
       this.#conditionalUiAbortController = null;
 
@@ -350,8 +323,8 @@ class DescopeWc extends BaseDescopeWc {
       try {
         response =
           action === RESPONSE_ACTIONS.webauthnCreate
-            ? await this.sdk.webauthn.helpers.create(options)
-            : await this.sdk.webauthn.helpers.get(options);
+            ? await this.sdk.webauthn.helpers.create(webauthnOptions)
+            : await this.sdk.webauthn.helpers.get(webauthnOptions);
       } catch (e) {
         if (e.name !== 'NotAllowedError') {
           this.loggerWrapper.error(e.message);
