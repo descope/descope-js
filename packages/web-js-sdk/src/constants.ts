1395ef7466a194d97616d0cb6ff4859c22a7492e
Asaf Shen <asaf@descope.com>
2023-10-02 19:18:38 +0300

    fix large expiration session

--- packages/web-js-sdk/src/constants.ts
+++ packages/web-js-sdk/src/constants.ts
@@ -1,2 +1,6 @@
 // This sdk can be used in SSR apps
 export const IS_BROWSER = typeof window !== 'undefined';
+
+// Maximum timeout value for setTimeout
+// For more information, refer to https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value
+export const MAX_TIMEOUT = Math.pow(2, 31) - 1;
