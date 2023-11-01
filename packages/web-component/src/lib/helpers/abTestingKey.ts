3d1f27f387f53f0cdb95e188c9b7788cf180d529
aviadl <aviad@descope.com>
2023-11-01 14:58:02 +0200

    AB testing client condition calculation Also now we know how to handle predicates in conditions + tests fixes https://github.com/descope/etc/issues/2333

---
+++ packages/web-component/src/lib/helpers/abTestingKey.ts
@@ -0,0 +1,11 @@
+const LOCAL_STORAGE_AB_TESTING_KEY = 'dls_ab_testing_id';
+
+export const getABTestingKey = (): number => {
+  const abTestingKey = localStorage.getItem(LOCAL_STORAGE_AB_TESTING_KEY);
+  if (!abTestingKey) {
+    const generatedKey = Math.floor(Math.random() * 100 + 1);
+    localStorage.setItem(LOCAL_STORAGE_AB_TESTING_KEY, generatedKey.toString());
+    return generatedKey;
+  }
+  return Number(abTestingKey);
+};
