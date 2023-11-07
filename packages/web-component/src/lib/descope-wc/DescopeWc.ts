0ac9314fe7476ece850f77b3960848b07b9cf33e
Tomer Lichtash <tomer@descope.com>
2023-11-07 10:53:17 +0200

    fix: Input query selector (#300)
    ## Related Issues
    
    Fixes https://github.com/descope/etc/issues/4584
    
    ## Related PRs
    
    | branch       | PR         |
    | ------------ | ---------- |
    | service a PR | Link to PR |
    | service b PR | Link to PR |
    
    ## Description
    
    A few sentences describing the overall goals of the pull request's
    commits.
    
    ## Must
    
    - [ ] Tests
    - [ ] Documentation (if applicable)

--- packages/web-component/src/lib/descope-wc/DescopeWc.ts
+++ packages/web-component/src/lib/descope-wc/DescopeWc.ts
@@ -698,7 +698,7 @@ class DescopeWc extends BaseDescopeWc {
   }
 
   #validateInputs() {
-    return Array.from(this.shadowRoot.querySelectorAll('.descope-input')).every(
+    return Array.from(this.shadowRoot.querySelectorAll('*[name]')).every(
       (input: HTMLInputElement) => {
         input.reportValidity();
         return input.checkValidity();
@@ -709,7 +709,7 @@ class DescopeWc extends BaseDescopeWc {
   async #getFormData() {
     const inputs = Array.from(
       this.shadowRoot.querySelectorAll(
-        `.descope-input[name]:not([${DESCOPE_ATTRIBUTE_EXCLUDE_FIELD}])`
+        `*[name]:not([${DESCOPE_ATTRIBUTE_EXCLUDE_FIELD}])`
       )
     ) as HTMLInputElement[];
 
