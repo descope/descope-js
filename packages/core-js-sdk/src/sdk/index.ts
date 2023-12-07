9694f1018f6b49f6756244cd3b9bb22c8ae4c993
Asaf Shen <asaf@descope.com>
2023-12-07 14:12:20 +0200

    chore: add some docs (#328)
    ## Description
    adding some top level docs for our top level functions, other functions
    may need as well but I found those less trivial and simpler to doc
    (according to go-sdk docs)

--- packages/core-js-sdk/src/sdk/index.ts
+++ packages/core-js-sdk/src/sdk/index.ts
@@ -27,7 +27,7 @@ import { isString, isStringOrUndefined } from './validations/validators';
 
 const withJwtValidations = withValidations(stringNonEmpty('token'));
 const withOptionalTokenValidations = withValidations(
-  isStringOrUndefinedValidator('token')
+  isStringOrUndefinedValidator('token'),
 );
 
 /** Returns Descope SDK with all available operations */
@@ -42,32 +42,80 @@ export default (httpClient: HttpClient) => ({
   webauthn: withWebauthn(httpClient),
   password: withPassword(httpClient),
   flow: withFlow(httpClient),
+  /**
+   * Refreshes a session token
+   * Should be called when a session has expired (failed validation) to renew it
+   * @param token A valid refresh token
+   * @returns The updated authentication info (JWTs)
+   */
   refresh: withOptionalTokenValidations((token?: string) =>
     transformResponse<JWTResponse>(
-      httpClient.post(apiPaths.refresh, {}, { token })
-    )
+      httpClient.post(apiPaths.refresh, {}, { token }),
+    ),
   ),
+  /**
+   * Selects a tenant for the current session
+   * @param tenantId The tenant to select
+   * @param token A valid refresh token
+   * @returns The updated authentication info (JWTs). The session token will be updated with the selected tenant under the "dct" claim
+   */
   selectTenant: withValidations(
     [isString('tenantId')],
-    [isStringOrUndefined('"token" must be string or undefined')]
+    [isStringOrUndefined('"token" must be string or undefined')],
   )((tenantId: string, token?: string) =>
     transformResponse<JWTResponse>(
-      httpClient.post(apiPaths.selectTenant, { tenant: tenantId }, { token })
-    )
+      httpClient.post(apiPaths.selectTenant, { tenant: tenantId }, { token }),
+    ),
   ),
-
+  /**
+   * Logs out the current session
+   * @param token A valid refresh token
+   */
   logout: withOptionalTokenValidations((token?: string) =>
-    transformResponse<never>(httpClient.post(apiPaths.logout, {}, { token }))
+    transformResponse<never>(httpClient.post(apiPaths.logout, {}, { token })),
   ),
+  /**
+   * Logs out all sessions for the current user
+   * @param token A valid refresh token
+   */
   logoutAll: withOptionalTokenValidations((token?: string) =>
-    transformResponse<never>(httpClient.post(apiPaths.logoutAll, {}, { token }))
+    transformResponse<never>(
+      httpClient.post(apiPaths.logoutAll, {}, { token }),
+    ),
   ),
+  /**
+   * Returns the current user details
+   * @param token A valid refresh token
+   * @returns The current user details
+   */
   me: withOptionalTokenValidations((token?: string) =>
-    transformResponse<UserResponse>(httpClient.get(apiPaths.me, { token }))
+    transformResponse<UserResponse>(httpClient.get(apiPaths.me, { token })),
   ),
+  /**
+   * Checks if the given JWT is still valid but DOES NOT check for signature
+   * @param token A valid token
+   * @returns true if the JWT is expired, false otherwise
+   */
   isJwtExpired: withJwtValidations(isJwtExpired),
+  /**
+   * Returns the list of tenants in the given JWT but DOES NOT check for signature
+   * @param token A valid token
+   * @returns The list of tenants in the given JWT
+   */
   getTenants: withJwtValidations(getTenants),
+  /**
+   * Returns the list of permissions granted in the given JWT but DOES NOT check for signature
+   * @param token A valid token
+   * @param tenant The tenant to check permissions for. If not provided, the permissions for the current tenant will be returned
+   * @returns The list of permissions granted in the given JWT
+   */
   getJwtPermissions: withJwtValidations(getJwtPermissions),
+  /**
+   * Returns the list of roles specified in the given JWT but DOES NOT check for signature
+   * @param token A valid token
+   * @param tenant The tenant to check roles for. If not provided, the roles for the current tenant will be returned
+   * @returns The list of roles specified in the given JWT
+   */
   getJwtRoles: withJwtValidations(getJwtRoles),
   httpClient,
 });
