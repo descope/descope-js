7df55cecd9fe26ba3382db181a0c78d13b6155a6
Eliya Sadan <eliya@descope.com>
2023-12-31 21:12:24 +0200

    feat: Extract region from project ID (#344)
    ## Related Issues
    
    Fixes https://github.com/descope/etc/issues/5120

--- packages/core-js-sdk/src/httpClient/urlBuilder.ts
+++ packages/core-js-sdk/src/httpClient/urlBuilder.ts
@@ -1,13 +1,19 @@
+import { BASE_URL_REGION_PLACEHOLDER } from '../constants';
+
 /** Build URL with given parts */
 export const urlBuilder = ({
   path,
   baseUrl,
   queryParams,
+  projectId,
 }: {
   path: string;
   baseUrl: string;
   queryParams: ConstructorParameters<typeof URLSearchParams>[0];
+  projectId: string;
 }) => {
+  const region = projectId.slice(1, -27);
+  baseUrl = baseUrl.replace(BASE_URL_REGION_PLACEHOLDER, region ? region + '.' : '');
   const url = new URL(path, baseUrl);
   if (queryParams) url.search = new URLSearchParams(queryParams).toString();
 
