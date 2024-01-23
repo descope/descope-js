d608a52c4731946105d15a683adede84de155c04
Nir Gur Arie <nir@descope.com>
2024-01-23 21:40:17 +0200

    cleanup for first release

--- packages/user-management-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initNotificationsMixin.ts
+++ packages/user-management-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initNotificationsMixin.ts
@@ -10,55 +10,65 @@ import warning from '../../../../assets/warning.svg';
 import close from '../../../../assets/close.svg';
 import { initLifecycleMixin } from '../../../../mixins/initLifecycleMixin';
 
-type Notifications = ReturnType<typeof getNotifications>
-type Notification = Notifications[0]
+type Notifications = ReturnType<typeof getNotifications>;
+type Notification = Notifications[0];
 
-export const initNotificationsMixin = createSingletonMixin(<T extends CustomElementConstructor>(superclass: T) =>
-  class InitNotificationsMixinClass extends compose(loggerMixin, stateManagementMixin, notificationsMixin, initLifecycleMixin)(superclass) {
+const notificationTypesIcons = {
+  success: checkmark(),
+  error: warning(),
+};
 
-    // eslint-disable-next-line class-methods-use-this
-    #createNotificationContent({ type, msg }: Notification) {
-      // eslint-disable-next-line no-nested-ternary
-      const icon = type === 'success' ? checkmark() :
-        type === 'error' ? warning()
-          : null;
+export const initNotificationsMixin = createSingletonMixin(
+  <T extends CustomElementConstructor>(superclass: T) =>
+    class InitNotificationsMixinClass extends compose(
+      loggerMixin,
+      stateManagementMixin,
+      notificationsMixin,
+      initLifecycleMixin,
+    )(superclass) {
+      // eslint-disable-next-line class-methods-use-this
+      #createNotificationContent({ type, msg }: Notification) {
+        const icon = notificationTypesIcons[type];
 
-      const closeIcon = Object.assign(close(), { slot: 'close' });
+        const closeIcon = Object.assign(close(), { slot: 'close' });
 
-      return `
-      ${icon?.outerHTML}${msg}
-      ${closeIcon.outerHTML}
-      `;
-    }
-
-    #createNotification(type: Notification['type']) {
-      return this.createNotification({
-        mode: type,
-        duration: 3000,
-        position: 'bottom-start',
-        size: 'sm',
-      });
-    }
-
-    #onNotificationsUpdate = withMemCache((notifications: Notifications) => {
-      if (notifications.length) {
-        notifications.forEach(({ type, msg }) => {
-          const notification = this.#createNotification(type);
-
-          notification.setContent(this.#createNotificationContent({ type, msg }));
-
-          notification.show();
-        });
-
-        // when there is a selection update from the table we get a double notification
-        // this is why we are wrapping the clearNotifications action with timeout;
-        setTimeout(() => this.actions.clearNotifications());
+        return `${icon?.outerHTML || ''}${msg}${closeIcon.outerHTML}`;
       }
-    });
 
-    async init() {
-      await super.init?.();
+      #createNotification(type: Notification['type']) {
+        return this.createNotification({
+          mode: type,
+          duration: 3000,
+          position: 'bottom-start',
+          size: 'sm',
+        });
+      }
 
-      this.subscribe(this.#onNotificationsUpdate.bind(this), getNotifications);
-    }
-  });
+      #onNotificationsUpdate = withMemCache((notifications: Notifications) => {
+        if (notifications.length) {
+          notifications.forEach(({ type, msg }) => {
+            const notification = this.#createNotification(type);
+
+            notification.setContent(
+              this.#createNotificationContent({ type, msg }),
+            );
+
+            notification.show();
+          });
+
+          // when there is a selection update from the table we get a double notification
+          // this is why we are wrapping the clearNotifications action with timeout;
+          setTimeout(() => this.actions.clearNotifications());
+        }
+      });
+
+      async init() {
+        await super.init?.();
+
+        this.subscribe(
+          this.#onNotificationsUpdate.bind(this),
+          getNotifications,
+        );
+      }
+    },
+);
