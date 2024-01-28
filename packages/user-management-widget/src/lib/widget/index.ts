6fea7db772f9e3b5be644ad1d5e056d29b27ef88
Nir Gur Arie <nir@descope.com>
2024-01-28 21:53:11 +0200

    fixes

--- packages/user-management-widget/src/lib/widget/index.ts
+++ packages/user-management-widget/src/lib/widget/index.ts
@@ -1,17 +1,6 @@
 import { compose } from '../helpers/compose';
 import { initMixin } from './mixins/initMixin/initMixin';
 
-declare global {
-  interface HTMLElement {
-    attributeChangedCallback(
-      attrName: string,
-      oldValue: string | null,
-      newValue: string | null,
-    ): void;
-    connectedCallback(): void;
-  }
-}
-
 const rootMixin = (superclass: CustomElementConstructor) =>
   class RootMixinClass extends compose(initMixin)(superclass) {};
 
