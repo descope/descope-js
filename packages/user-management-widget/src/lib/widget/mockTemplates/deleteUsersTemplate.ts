e8494b027dda65f3f94ecea9bc118c26491995db
Tomer Lichtash <tomer@descope.com>
2024-01-29 19:30:39 +0200

    fix: Widget UI alignment (#367)
    ## Related Issues
    
    Fixes <link_to_github_issue>
    
    ## Related PRs
    
    | branch       | PR         |
    | ------------ | ---------- |
    | service a PR | Link to PR |
    | service b PR | Link to PR |
    
    ## Description
    
    A few sentences describing the overall goals of the pull request's
    commits.
    
    ## Must
    
    - [ ] Tests
    - [ ] Documentation (if applicable)

--- packages/user-management-widget/src/lib/widget/mockTemplates/deleteUsersTemplate.ts
+++ packages/user-management-widget/src/lib/widget/mockTemplates/deleteUsersTemplate.ts
@@ -7,12 +7,13 @@ export default `
   st-background-color="none"
   st-horizontal-padding="0rem"
   st-vertical-padding="0rem"
+  st-border-radius="sm"
   data-testid="delete-users-modal"
 >
-  <descope-text variant="h3" mode="surface">Delete Users</descope-text>
+  <descope-text variant="subtitle1" mode="surface">Delete Users</descope-text>
   <descope-text data-id="body-text" variant="body1" mode="surface">Delete user</descope-text>
 
-  <descope-container
+  <descope-container  
     direction="row"
     st-justify-content="flex-end"
     st-gap="1rem"
@@ -20,8 +21,8 @@ export default `
     st-horizontal-padding="0rem"
     st-vertical-padding="0rem"
   >
-  <descope-button data-testid="delete-users-modal-cancel" data-id="modal-cancel" mode="primary" variant="outline" size="md">Cancel</descope-button>
-  <descope-button data-testid="delete-users-modal-submit" data-id="modal-submit" mode="primary" variant="contained" size="md">Delete</descope-button>
+  <descope-button data-testid="delete-users-modal-cancel" data-id="modal-cancel" mode="primary" variant="link" size="md">Cancel</descope-button>
+  <descope-button data-testid="delete-users-modal-submit" data-id="modal-submit" mode="error" variant="contained" size="md">Delete</descope-button>
   </descope-container>
 </descope-container>
 `;
