2a4240771afd7173ed740ba131119142ee1ca8e1
Tomer Lichtash <tomer@descope.com>
2024-01-31 10:28:03 +0200

    fix: Widget container background color (#370)
    ## Related Issues
    
    Fixes <link_to_github_issue>
    
    ## Related PRs
    
    | branch       | PR         |
    | ------------ | ---------- |
    | service a PR | Link to PR |
    | service b PR | Link to PR |
    
    ## Description
    
    A few sentences describing the overall goals of the pull request's
    commits.
    
    ## Must
    
    - [ ] Tests
    - [ ] Documentation (if applicable)

--- packages/user-management-widget/src/lib/widget/mockTemplates/createUserTemplate.ts
+++ packages/user-management-widget/src/lib/widget/mockTemplates/createUserTemplate.ts
@@ -7,6 +7,7 @@ export default `
   st-background-color="none"
   st-horizontal-padding="0rem"
   st-vertical-padding="0rem"
+  st-border-radius="sm"
 >
   <descope-text variant="subtitle1" mode="surface">New User</descope-text>
   <descope-text-field name="loginId" label="Login ID" bordered="true" required="true" size="sm"></descope-text-field>
@@ -21,6 +22,7 @@ export default `
     st-host-width="100%"
     st-horizontal-padding="0.75rem"
     st-vertical-padding="0.75rem"
+    st-background-color="none"
   >
   <descope-button data-testid="create-user-modal-cancel" data-id="modal-cancel" mode="primary" variant="link" size="sm">Cancel</descope-button>
   <descope-button data-testid="create-user-modal-submit" data-id="modal-submit" mode="primary" variant="contained" size="sm">Create</descope-button>
