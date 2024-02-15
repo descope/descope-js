bec3de902fdef57bb53b6d82ad0ca9a3df9feec0
Tomer Lichtash <tomer@descope.com>
2024-02-15 11:30:48 +0200

    Fix role sort

--- packages/user-management-widget/src/lib/widget/drivers/MultiSelectDriver.ts
+++ packages/user-management-widget/src/lib/widget/drivers/MultiSelectDriver.ts
@@ -4,7 +4,7 @@ export class MultiSelectDriver extends BaseDriver {
   nodeName = 'descope-multi-select-combo-box';
 
   setData(data: { label: string; value: string }[]) {
-    this.ele?.setAttribute('data', JSON.stringify(data));
+    this.ele?.setAttribute('data', JSON.stringify(data.toSorted()));
   }
 
   setDefaultValues(vals: Record<string, string>) {
