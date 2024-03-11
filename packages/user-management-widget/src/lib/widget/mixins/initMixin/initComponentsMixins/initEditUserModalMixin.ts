1f9ddab1a93b0b6ee2aa94f465f5cd5b5429ae75
Tomer Lichtash <tomer@descope.com>
2024-03-11 12:34:30 +0200

    generic flatten/unflatten for custom attrs

--- packages/user-management-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initEditUserModalMixin.ts
+++ packages/user-management-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initEditUserModalMixin.ts
@@ -14,19 +14,29 @@ import { stateManagementMixin } from '../../stateManagementMixin';
 import { initWidgetRootMixin } from './initWidgetRootMixin';
 import { User } from '../../../api/types';
 
-const handleCustomAttributes = (formData: Partial<User>) =>
+const unflattenKeys = ['customAttributes'];
+
+const unflatten = (formData: Partial<User>) =>
   Object.entries(formData).reduce((acc, [key, value]) => {
-    if (key.startsWith('customAttributes.')) {
-      const customKey = key.split('.')[1];
-      if (!acc['customAttributes']) {
-        acc['customAttributes'] = {};
-      }
-      acc['customAttributes'][customKey] = value;
-    } else {
-      acc[key] = value;
+    const [prefix, ...rest] = key.split('.');
+
+    if (!unflattenKeys.includes(prefix)) {
+      return Object.assign(acc, { [key]: value });
     }
+
+    if (!acc[prefix]) {
+      acc[prefix] = {};
+    }
+
+    acc[prefix][rest.join('.')] = value;
+
     return acc;
   }, {});
+
+const flatten = (
+  vals: Record<string, string | boolean | number>,
+  keyPrefix: string,
+) => Object.entries(vals).map(([key, val]) => [`${keyPrefix}${key}`, val]);
 
 const formatPhoneNumber = (phoneNumber: string) => {
   if (!phoneNumber) return phoneNumber;
@@ -78,7 +88,7 @@ export const initEditUserModalMixin = createSingletonMixin(
             this.actions.updateUser({
               // we are joining the ids in order to display it so we need to split it back
               loginId: loginId.split(', ')[0],
-              ...handleCustomAttributes(formData),
+              ...unflatten(formData),
             });
             this.editUserModal.close();
             this.resetFormData(this.editUserModal.ele);
@@ -104,12 +114,7 @@ export const initEditUserModalMixin = createSingletonMixin(
           email: userDetails?.email,
           phone: formatPhoneNumber(userDetails?.phone),
           roles: userDetails?.roles,
-          ...Object.fromEntries(
-            Object.entries(userDetails.customAttributes).map(([key, val]) => [
-              `customAttributes.${key}`,
-              val,
-            ]),
-          ),
+          ...flatten(userDetails.customAttributes, 'customAttributes.'),
         };
 
         this.setFormData(this.editUserModal.ele, formData);
