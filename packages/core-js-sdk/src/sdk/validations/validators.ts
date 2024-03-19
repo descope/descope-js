007734f949f23bb48bf0a3bd427a07eafee88c23
Asaf Shen <asaf@descope.com>
2024-03-19 15:53:01 +0200

    fix: polyfil lodash get (#439) RELEASE
    ## Related Issues
    
    https://github.com/descope/etc/issues/5940
    
    
    ## Description
    replace lodash get with [the following
    impelmentation](https://gist.github.com/dfkaye/59263b51cf1e0b633181c5f44ae2066a)
    so package may run on various
    
    tested web-component sample app
    
    ![image](https://github.com/descope/descope-js/assets/10514677/eb7d6349-7025-4262-bc5e-7b2820b88042)
    
    and also web-js sample app
    
    ![image](https://github.com/descope/descope-js/assets/10514677/871a7c9e-ea46-4a9c-97fd-b1c20cf9f11e)

--- packages/core-js-sdk/src/sdk/validations/validators.ts
+++ packages/core-js-sdk/src/sdk/validations/validators.ts
@@ -1,4 +1,3 @@
-import get from 'lodash.get';
 import { createOrValidator, createValidation, createValidator } from './core';
 import { Validator } from './types';
 
@@ -9,8 +8,41 @@ const validateString = (val: any) => typeof val === 'string';
 const validateUndefined = (val: any) => val === undefined;
 
 const validateEmail = regexMatch(
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 );
+
+// A replacement for lodash.get, because it may not integrate well in various runtime environments (Edge).
+// Implementation is based on https://gist.github.com/dfkaye/59263b51cf1e0b633181c5f44ae2066a
+const get = (object: any, pathName: string, defaultValue?: any) => {
+  // Coerce pathName to a string (even it turns into "[object Object]").
+  const path = Array.isArray(pathName) ? pathName.join('.') : String(pathName);
+
+  // Support bracket notation, e.g., "a[0].b.c".
+  const match = /\[\\?("|')?(\w|d)+\\?("|')?\]/g;
+
+  const parts = path.replace(match, (m, i, v) => '.' + v).split('.');
+
+  const length = parts.length;
+  let i = 0;
+
+  // In case object isn't a real object, set it to undefined.
+  let value = object === Object(object) ? object : undefined;
+
+  while (value != null && i < length) {
+    value = value[parts[i++]];
+  }
+
+  /**
+   * returns the resolved value if
+   * 1. iteration happened (i > 0)
+   * 2. iteration completed (i === length)
+   * 3. the value at the path is found in the data structure (not undefined). Note that if the path is found but the
+   *    value is null, then null is returned.
+   * If any of those checks fails, return the defaultValue param, if provided.
+   */
+  return i && i === length && value !== undefined ? value : defaultValue;
+};
+
 const validatePhone = regexMatch(/^\+[1-9]{1}[0-9]{3,14}$/);
 const validateMinLength = (min: number) => (val: any) => val.length >= min;
 // const validatePlainObject = (val: any) => !!val && Object.getPrototypeOf(val) === Object.prototype;
@@ -19,29 +51,29 @@ const validatePathValue = (path: string, rules: Validator[]) => (val: any) =>
 
 export const isEmail = createValidator(
   validateEmail,
-  '"{val}" is not a valid email'
+  '"{val}" is not a valid email',
 );
 export const isPhone = createValidator(
   validatePhone,
-  '"{val}" is not a valid phone number'
+  '"{val}" is not a valid phone number',
 );
 export const isNotEmpty = createValidator(
   validateMinLength(1),
-  'Minimum length is 1'
+  'Minimum length is 1',
 );
 export const isString = createValidator(
   validateString,
-  'Input is not a string'
+  'Input is not a string',
 );
 
 export const isUndefined = createValidator(
   validateUndefined,
-  'Input is defined'
+  'Input is defined',
 );
 
 export const isStringOrUndefined = createOrValidator(
   [isString(), isUndefined()],
-  'Input is not a string or undefined'
+  'Input is not a string or undefined',
 );
 
 // export const isPlainObject = createValidator(validatePlainObject, 'Input is not a plain object');
