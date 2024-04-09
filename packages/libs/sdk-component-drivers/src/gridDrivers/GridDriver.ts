25018436788f563e3e84abe9bc65309f176d9a7e
Nir Gur Arie <nir@descope.com>
2024-04-09 21:59:53 +0300

    fix issue 6179

--- packages/libs/sdk-component-drivers/src/gridDrivers/GridDriver.ts
+++ packages/libs/sdk-component-drivers/src/gridDrivers/GridDriver.ts
@@ -58,7 +58,7 @@ export class GridDriver<T extends any> extends BaseDriver {
   }
 
   filterColumns(filterFn: (col: Column) => boolean) {
-    const filteredColumns = this.ele.columns.filter(filterFn);
+    const filteredColumns = this.ele.columns?.filter(filterFn);
     if (!compareArrays(filteredColumns, this.ele.columns)) {
       this.ele.columns = filteredColumns;
       this.#onColumnsChangeCb?.(filteredColumns);
