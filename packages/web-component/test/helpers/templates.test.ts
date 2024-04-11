a0b3d50f4256323125c81008e1daeb4b2ef5e95b
Asaf Shen <asaf@descope.com>
2024-04-11 23:36:02 +0300

    test

--- packages/web-component/test/helpers/templates.test.ts
+++ packages/web-component/test/helpers/templates.test.ts
@@ -1,6 +1,9 @@
 import { waitFor } from '@testing-library/dom';
 import { screen } from 'shadow-dom-testing-library';
-import { updateScreenFromScreenState } from '../../src/lib/helpers/templates';
+import {
+  setNOTPVariable,
+  updateScreenFromScreenState,
+} from '../../src/lib/helpers/templates';
 
 describe('templates', () => {
   afterEach(() => {
@@ -29,5 +32,37 @@ describe('templates', () => {
       form: { email: 'email2' },
     });
     await waitFor(() => screen.getByShadowDisplayValue('email2'));
+  });
+
+  it('should set NOTP variable', async () => {
+    document.body.innerHTML = `<div> </div>`;
+    const image = 'image';
+    const urlVar = 'url-var';
+
+    // Mock customElements.get
+    const originalGet = customElements.get;
+    customElements.get = jest.fn().mockReturnValue({
+      cssVarList: {
+        url: urlVar,
+      },
+    });
+
+    // Mock HTMLElement.style.setProperty
+    const setPropertySpy = jest.spyOn(
+      CSSStyleDeclaration.prototype,
+      'setProperty',
+    );
+
+    setNOTPVariable(document.body, image);
+
+    expect(customElements.get).toHaveBeenCalledWith('descope-notp-image');
+    expect(setPropertySpy).toHaveBeenCalledWith(
+      urlVar,
+      `url(data:image/jpg;base64,${image})`,
+    );
+
+    // Restore original functions
+    customElements.get = originalGet;
+    setPropertySpy.mockRestore();
   });
 });
