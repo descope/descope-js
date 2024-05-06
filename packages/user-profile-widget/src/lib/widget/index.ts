e6bc3e9298a9e737ec3890680474561c903a6c04
Doron Sharon <dorsha@descope.com>
2024-05-06 15:18:09 +0300

    Add widget headers to flow (#564)

--- packages/user-profile-widget/src/lib/widget/index.ts
+++ packages/user-profile-widget/src/lib/widget/index.ts
@@ -1,7 +1,21 @@
 import { compose } from '@descope/sdk-helpers';
+import WebComponent from '@descope/web-component';
 import { initMixin } from './mixins/initMixin/initMixin';
 
+declare const BUILD_VERSION: string;
+
 const rootMixin = (superclass: CustomElementConstructor) =>
-  class RootMixinClass extends initMixin(superclass) {};
+  class RootMixinClass extends initMixin(superclass) {
+    async init() {
+      await super.init?.();
+      WebComponent.sdkConfigOverrides = {
+        baseHeaders: {
+          'x-descope-widget-type': 'user-profile-widget',
+          'x-descope-widget-id': this.getAttribute('widget-id'),
+          'x-descope-widget-version': BUILD_VERSION,
+        },
+      };
+    }
+  };
 
 export const UserProfileWidget = compose(rootMixin)(HTMLElement);
