e456a23843e7e6730080df52643b3d473ce4eebc
nirgur <nirgur@users.noreply.github.com>
2024-05-06 16:21:52 +0300

    fix: UPW_redirect_flow RELEASE (#565)

--- packages/user-profile-widget/src/lib/widget/mixins/helpers.ts
+++ packages/user-profile-widget/src/lib/widget/mixins/helpers.ts
@@ -5,6 +5,7 @@ type FlowConfig = {
   flowId: string;
   baseUrl?: string;
 };
+
 export const createFlowTemplate = (
   flowConfig: FlowConfig = {} as FlowConfig,
 ) => {
@@ -18,3 +19,19 @@ export const createFlowTemplate = (
 
   return template;
 };
+
+export function getUrlParam(paramName: string) {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  return urlParams.get(paramName);
+}
+
+export function resetUrlParam(paramName: string) {
+  if (window.history.replaceState && getUrlParam(paramName)) {
+    const newUrl = new URL(window.location.href);
+    const search = new URLSearchParams(newUrl.search);
+    search.delete(paramName);
+    newUrl.search = search.toString();
+    window.history.replaceState({}, '', newUrl.toString());
+  }
+}
