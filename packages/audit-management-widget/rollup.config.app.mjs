5bcc922f3e56784259459b93684a869329a8a895
Nir Gur Arie <nir@descope.com>
2024-05-26 13:25:59 +0300

    use base url for static content in widgets

--- packages/audit-management-widget/rollup.config.app.mjs
+++ packages/audit-management-widget/rollup.config.app.mjs
@@ -24,8 +24,8 @@ export default {
           process.env.DESCOPE_PROJECT_ID || '',
         ),
         DESCOPE_BASE_URL: JSON.stringify(process.env.DESCOPE_BASE_URL || ''),
-        DESCOPE_STATIC_BASE_URL: JSON.stringify(
-          process.env.DESCOPE_STATIC_BASE_URL || '',
+        DESCOPE_BASE_STATIC_URL: JSON.stringify(
+          process.env.DESCOPE_BASE_STATIC_URL || '',
         ),
         DESCOPE_TENANT: JSON.stringify(process.env.DESCOPE_TENANT || ''),
         DESCOPE_WIDGET_ID: JSON.stringify(process.env.DESCOPE_WIDGET_ID || ''),
