798aa909c74342f54cd7b69c56ac109d8fa4d367
Asaf Shen <asaf@descope.com>
2024-05-29 16:14:10 +0300

    handle logout

--- packages/web-component/src/app-web-sample/index.js
+++ packages/web-component/src/app-web-sample/index.js
@@ -7,21 +7,29 @@ const sdk = createSdk({
   autoRefresh: true,
 });
 
+const loginContainer = document.querySelector('.login-container');
+
 async function main() {
-  // check if user is already authenticated
-  const sessionToken = sdk.getSessionToken();
-  const userAuthenticated = !!sessionToken && !sdk.isJwtExpired(sessionToken);
+  // show loading message
+  const loadingDiv = document.createElement('div');
+  loadingDiv.textContent = 'Loading...';
+  loginContainer.appendChild(loadingDiv);
+
+  // first try to refresh token, this will also trigger the auto-refresh mechanism
+  const refreshRes = await sdk.refresh();
+  let userAuthenticated = refreshRes.ok;
   if (userAuthenticated) {
     // user is authenticated
     console.log('User is authenticated');
+    // remove loading message
+    loadingDiv.remove();
+
     // get user name
     const userRes = await sdk.me();
     if (!userRes.ok) {
       console.error('Error fetching user details', userRes.error);
       // add a div with error message
-      const errorDiv = document.createElement('div');
-      errorDiv.textContent = 'Error fetching user details';
-      loginContainer.appendChild(errorDiv);
+      addErrorMessageToPage('Error fetching user details');
       return;
     }
     // add a div with user name and logout button
@@ -30,8 +38,44 @@ async function main() {
   }
   // user is not authenticated
   console.log('User is not authenticated');
-  // get loginContainer by class login-container
-  const loginContainer = document.querySelector('.login-container');
+  // remove loading message
+  loadingDiv.remove();
+
+  // show login flow
+  addDescopeFlowToPage();
+}
+
+async function addUserToPage(user) {
+  const userDiv = document.createElement('div');
+  userDiv.textContent = `Welcome ${user?.name || user?.loginIds?.[0] || ''}`;
+  loginContainer.appendChild(userDiv);
+  const logoutButton = document.createElement('button');
+  logoutButton.textContent = 'Logout';
+  logoutButton.onclick = async () => {
+    await sdk.logout();
+    // reload the page
+    window.location.reload();
+  };
+  // add logout button to login container
+  loginContainer.appendChild(logoutButton);
+
+  // in case of session token is missing, reload the page
+  // this may happen if the user logs out from another tab or the refresh token is revoked/expired
+  sdk.onSessionTokenChange = async (token) => {
+    if (!token) {
+      // reload the page
+      window.location.reload();
+    }
+  };
+}
+
+async function addErrorMessageToPage(message) {
+  const errorDiv = document.createElement('div');
+  errorDiv.textContent = message;
+  loginContainer.appendChild(errorDiv);
+}
+
+function addDescopeFlowToPage() {
   const descopeWc = document.createElement('descope-wc');
   descopeWc.setAttribute('project-id', process.env.DESCOPE_PROJECT_ID);
   descopeWc.setAttribute(
@@ -55,35 +99,8 @@ async function main() {
   descopeWc.addEventListener('error', (event) => {
     console.error('Error logging in', event.detail);
     // add a div with error message
-    const errorDiv = document.createElement('div');
-    errorDiv.textContent = 'Error logging in';
-    loginContainer.appendChild(errorDiv);
-    // remove login container
-    loginContainer.remove();
+    addErrorMessageToPage('Error logging in');
   });
-
-  return;
-}
-
-async function addUserToPage(user) {
-  const loginContainer = document.querySelector('.login-container');
-
-  const userDiv = document.createElement('div');
-  userDiv.textContent = `Welcome ${user?.name || user?.loginIds?.[0] || ''}`;
-  loginContainer.appendChild(userDiv);
-  const logoutButton = document.createElement('button');
-  logoutButton.textContent = 'Logout';
-  logoutButton.onclick = async () => {
-    const logoutRes = await sdk.logout();
-    if (!logoutRes.ok) {
-      console.error('Error logging out', logoutRes.error);
-      return;
-    }
-    // reload the page
-    window.location.reload();
-  };
-  // add logout button to login container
-  loginContainer.appendChild(logoutButton);
 }
 
 main();
