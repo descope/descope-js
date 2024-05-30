a0942c8b3fec7350c520dcf301dd5d6e269854d5
Asaf Shen <asaf@descope.com>
2024-05-30 17:23:36 +0300

    feat: get auth claims by current tenant if it matches (#612)
    ## Description
    get auth claims by current tenant for `getJwtPermissions` and
    `getJwtRoles`
    
    with the following sample code
    <img width="1096" alt="image"
    src="https://github.com/descope/descope-js/assets/10514677/eb877154-fd80-434c-9981-4637d6e1ef46">
    
    
    
    and the following jwt
    
    ![image](https://github.com/descope/descope-js/assets/10514677/0d3b947c-98e7-4260-985c-46ec061d379e)
    
    
    here is the result
    <img width="408" alt="Screenshot 2024-05-30 at 17 07 05"
    src="https://github.com/descope/descope-js/assets/10514677/b83ec25f-3c1a-456e-8225-3bc5fb97e020">

--- packages/core-js-sdk/src/constants/index.ts
+++ packages/core-js-sdk/src/constants/index.ts
@@ -7,5 +7,8 @@ export const MIN_POLLING_INTERVAL_MS = 1000; // 1 second
 /** Default maximum time we are willing to wait for the magic-link/enchanted-link/notp to be clicked */
 export const MAX_POLLING_TIMEOUT_MS = 1000 * 60 * 10; // 10 minutes
 
+/**  Descope current tenant claim */
+export const DESCOPE_CURRENT_TENANT_CLAIM = 'dct';
+
 /** API paths to the Descope service */
 export { default as apiPaths } from './apiPaths';
