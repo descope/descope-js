4ed46fb7bd70b27a70b5945026d00d65f506a07a
aviadl <aviad@descope.com>
2024-06-19 14:52:21 +0300

    Revert pnpm added a missing ; Added test

--- packages/web-component/test/descope-wc.test.ts
+++ packages/web-component/test/descope-wc.test.ts
@@ -577,7 +577,7 @@ describe('web-component', () => {
           client: {},
           redirectAuth: undefined,
           tenant: undefined,
-          locale: "en",
+          locale: 'en',
         },
         undefined,
         'submitterId',
@@ -882,6 +882,23 @@ describe('web-component', () => {
       timeout: WAIT_TIMEOUT,
     });
     await waitFor(() => screen.getByShadowText('hey john!'));
+  });
+
+  it('should update page href attribute according to screen state', async () => {
+    startMock.mockReturnValue(
+      generateSdkResponse({ screenState: { user: { name: 'john' } } }),
+    );
+
+    pageContent = `<div>Loaded1</div><descope-link class="descope-link" href="{{user.name}}">ho!</descope-link>`;
+
+    document.body.innerHTML = `<h1>Custom element test</h1> <descope-wc flow-id="otpSignInEmail" project-id="1"></descope-wc>`;
+
+    await waitFor(() => screen.getByShadowText('Loaded1'), {
+      timeout: WAIT_TIMEOUT,
+    });
+    await waitFor(() =>
+      expect(screen.getByShadowText('ho!')).toHaveAttribute('href', 'john'),
+    );
   });
 
   it('should update page templates according to last auth login ID when there is only login Id', async () => {
@@ -2114,7 +2131,7 @@ describe('web-component', () => {
             redirectAuth: undefined,
             preview: false,
             tenant: undefined,
-            locale: "en",
+            locale: 'en',
           },
           conditionInteractionId,
           'interactionId',
@@ -2166,7 +2183,7 @@ describe('web-component', () => {
             tenant: undefined,
             client: {},
             lastAuth: { authMethod: 'otp' },
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2260,7 +2277,7 @@ describe('web-component', () => {
             samlIdpUsername: null,
             ssoAppId: null,
             client: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2343,7 +2360,7 @@ describe('web-component', () => {
             },
             tenant: undefined,
             lastAuth: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2388,7 +2405,7 @@ describe('web-component', () => {
             samlIdpUsername: null,
             ssoAppId: null,
             client: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2427,7 +2444,7 @@ describe('web-component', () => {
             tenant: undefined,
             redirectAuth: undefined,
             lastAuth: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2555,7 +2572,7 @@ describe('web-component', () => {
             tenant: undefined,
             redirectAuth: undefined,
             lastAuth: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2596,7 +2613,7 @@ describe('web-component', () => {
             tenant: undefined,
             redirectAuth: undefined,
             lastAuth: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2634,7 +2651,7 @@ describe('web-component', () => {
             tenant: undefined,
             redirectAuth: undefined,
             lastAuth: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2705,7 +2722,7 @@ describe('web-component', () => {
             redirectAuth: undefined,
             lastAuth: {},
             client: {},
-            locale: "en",
+            locale: 'en',
           },
           undefined,
           '',
@@ -2747,7 +2764,7 @@ describe('web-component', () => {
           tenant: undefined,
           redirectAuth: undefined,
           lastAuth: {},
-          locale: "en",
+          locale: 'en',
         },
         undefined,
         '',
@@ -2831,7 +2848,7 @@ describe('web-component', () => {
           redirectAuth: undefined,
           tenant: undefined,
           lastAuth: {},
-          locale: "en",
+          locale: 'en',
         },
         undefined,
         '',
@@ -2896,7 +2913,7 @@ describe('web-component', () => {
           tenant: undefined,
           lastAuth: {},
           client: {},
-          locale: "en",
+          locale: 'en',
         },
         undefined,
         '',
