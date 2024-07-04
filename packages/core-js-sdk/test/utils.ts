792cff98e1d96efe10b6ea0dd62989d7d6bc20f0
Nicolas Macian <40004186+nmacianx@users.noreply.github.com>
2024-07-04 14:47:19 +0300

    feat: Add Patch method to HTTP client (#675) RELEASE
    ## Description
    
    Adds support for the Patch method to the HTTP client

--- packages/core-js-sdk/test/utils.ts
+++ packages/core-js-sdk/test/utils.ts
@@ -1,10 +1,11 @@
 export const mockHttpClient = {
   get: jest.fn(),
   post: jest.fn(),
+  patch: jest.fn(),
   put: jest.fn(),
   delete: jest.fn(),
   reset: () =>
-    ['get', 'post', 'put', 'delete'].forEach((key) =>
+    ['get', 'post', 'put', 'delete', 'patch'].forEach((key) =>
       mockHttpClient[key].mockResolvedValue({
         ok: true,
         json: () => Promise.resolve({ body: 'body' }),
