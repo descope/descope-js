f9914aa5127bc4fa41242706eb2b6db8aa0f45e6
Bar Saar <bar@descope.com>
2024-07-09 12:32:05 +0300

    lint

--- packages/web-component/src/lib/helpers/flowInputs.ts
+++ packages/web-component/src/lib/helpers/flowInputs.ts
@@ -1,7 +1,7 @@
 const flattenFormObject = (obj: any, prefix = '') =>
   Object.keys(obj).reduce((res, el) => {
     if (typeof obj[el] === 'object' && obj[el] !== null && !obj[el]?.value) {
-      return { ...res, ...flattenFormObject(obj[el], `${prefix + el  }.`) };
+      return { ...res, ...flattenFormObject(obj[el], `${prefix + el}.`) };
     }
     const v = typeof obj[el] === 'object' ? obj[el] : { value: obj[el] };
     return { ...res, [prefix + el]: v, [`form.${prefix}${el}`]: v };
