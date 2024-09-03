19e2cfde2fd061110fec8918e211f89909553f8a
Asaf Shen <asaf@descope.com>
2024-09-03 11:01:51 +0300

    fix: upgrade jwt decode to 4.0.0. (#789) RELEASE
    ## Related Issues
    
    fixes https://github.com/descope/etc/issues/7559
    
    
    
    ## Description
    upgrade jwt decode to 4.0.0

--- packages/sdks/core-js-sdk/test/sdk/sdk.test.ts
+++ packages/sdks/core-js-sdk/test/sdk/sdk.test.ts
@@ -1,10 +1,14 @@
 // @ts-nocheck
 import createSdk from '../../src/sdk';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import { mockHttpClient } from '../utils';
 import { apiPaths } from '../../src/constants';
 
-jest.mock('jwt-decode', () => jest.fn());
+jest.mock('jwt-decode', () => {
+  return {
+    jwtDecode: jest.fn(),
+  };
+});
 
 const sdk = createSdk(mockHttpClient);
 
