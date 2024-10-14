3085ddd4105f2d34f5b1bfcbc63b4e5f8d954eea
Tomer Lichtash <tomer@descope.com>
2024-10-14 15:11:15 +0300

    clear

--- packages/sdks/web-js-sdk/src/constants.ts
+++ packages/sdks/web-js-sdk/src/constants.ts
@@ -4,5 +4,3 @@ export const IS_BROWSER = typeof window !== 'undefined';
 // Maximum timeout value for setTimeout
 // For more information, refer to https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value
 export const MAX_TIMEOUT = Math.pow(2, 31) - 1;
-
-export const a = 1
