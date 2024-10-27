d9446ee230bfa15aee501e6f6c60788b8f091fd2
Omer Cohen <omer@descope.com>
2024-10-27 16:13:15 +0200

    Update constants.ts
    Co-authored-by: nirgur <nirgur@users.noreply.github.com>
    Signed-off-by: Omer Cohen <git@omerc.net>

--- packages/libs/sdk-mixins/src/mixins/descopeUiMixin/constants.ts
+++ packages/libs/sdk-mixins/src/mixins/descopeUiMixin/constants.ts
@@ -3,7 +3,7 @@ import { IS_LOCAL_STORAGE } from '../../constants';
 export const DESCOPE_UI_SCRIPT_ID = 'load-descope-ui';
 export const DESCOPE_UI_FALLBACK_SCRIPT_ID = 'load-descope-ui-fallback-script';
 export const DESCOPE_UI_FALLBACK_2_SCRIPT_ID =
-  'load-descope-ui-fallback-script';
+  'load-descope-ui-fallback-script-2';
 
 export const UI_COMPONENTS_URL_KEY = 'base.ui.components.url';
 
