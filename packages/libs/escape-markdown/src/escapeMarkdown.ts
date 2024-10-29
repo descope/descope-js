16e766d464f047dab61cd054cf167dc2345feddd
Tomer Lichtash <tomer@descope.com>
2024-10-29 12:59:40 +0200

    add unescape method

--- packages/libs/escape-markdown/src/escapeMarkdown.ts
+++ packages/libs/escape-markdown/src/escapeMarkdown.ts
@@ -6,3 +6,10 @@ export const escapeMarkdown = (s: string) => {
   const regexp = new RegExp(`(${escapedTextRegexp})`, 'g');
   return s.replace(regexp, '\\$1');
 };
+
+export const unescapeMarkdown = (s: string) => {
+  if (typeof s !== 'string') return s;
+  const escapedTextRegexp = mdChars.map((char) => `\\\\${char}`).join('|');
+  const regexp = new RegExp(`(${escapedTextRegexp})`, 'g');
+  return s.replace(regexp, (match) => match.slice(1));
+};
--- packages/libs/escape-markdown/test/escapeMarkdown.test.ts
+++ packages/libs/escape-markdown/test/escapeMarkdown.test.ts
@@ -1,4 +1,4 @@
-import { escapeMarkdown } from '../src';
+import { escapeMarkdown, unescapeMarkdown } from '../src';
 
 describe('escape markdown', () => {
   it('should escape markdown chars', () => {
@@ -9,5 +9,20 @@ describe('escape markdown', () => {
   it('ignore non-strings', () => {
     const source = 1234;
     expect(escapeMarkdown(source as any)).toEqual(source);
+  });
+});
+
+describe('unescape markdown', () => {
+  it('should unescape markdown chars', () => {
+    const source =
+      '\\*asterisk\\* \\_underscore\\_ \\(brackets1\\) \\[brackets2\\] \\`backtick\\`';
+    expect(unescapeMarkdown(source)).toEqual(
+      '*asterisk* _underscore_ (brackets1) [brackets2] `backtick`',
+    );
+  });
+
+  it('ignore non-strings', () => {
+    const source = 1234;
+    expect(unescapeMarkdown(source as any)).toEqual(source);
   });
 });
