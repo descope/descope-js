5221c58f23d11b14cec7986b7736275f7a8fb0ff
Eliya Sadan <eliya@descope.com>
2024-11-28 12:38:08 +0200

    Load dynamically

--- packages/sdks/web-js-sdk/src/constants.ts
+++ packages/sdks/web-js-sdk/src/constants.ts
@@ -4,3 +4,8 @@ export const IS_BROWSER = typeof window !== 'undefined';
 // Maximum timeout value for setTimeout
 // For more information, refer to https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value
 export const MAX_TIMEOUT = Math.pow(2, 31) - 1;
+
+export const OIDC_CLIENT_TS_DESCOPE_CDN_URL =
+  'https://descopecdn.com/npm/oidc-client-ts@3.1.0/dist/browser/oidc-client-ts.min.js';
+export const OIDC_CLIENT_TS_JSDELIVR_CDN_URL =
+  'https://cdn.jsdelivr.net/npm/oidc-client-ts@3.1.0/dist/browser/oidc-client-ts.min.js';
--- packages/sdks/web-js-sdk/src/sdk/oidc.ts
+++ packages/sdks/web-js-sdk/src/sdk/oidc.ts
@@ -1,11 +1,15 @@
 import { JWTResponse, SdkResponse, URLResponse } from '@descope/core-js-sdk';
-import {
+import type {
   OidcClient,
   OidcClientSettings,
   SigninResponse,
   WebStorageStateStore,
 } from 'oidc-client-ts';
 import { CoreSdk } from '../types';
+import {
+  OIDC_CLIENT_TS_DESCOPE_CDN_URL,
+  OIDC_CLIENT_TS_JSDELIVR_CDN_URL,
+} from '../constants';
 
 interface OidcConfig {
   clientId: string;
@@ -13,13 +17,75 @@ interface OidcConfig {
   scope: string;
 }
 
-const getOidcClient = (
+type OidcModule = {
+  OidcClient: typeof OidcClient;
+  WebStorageStateStore: typeof WebStorageStateStore;
+};
+
+let scriptLoadingPromise: Promise<OidcModule>;
+
+const simpleHash = (input: string): string => {
+  let hash = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash; // Convert to 32-bit integer
+  }
+
+  return Math.abs(hash).toString(16); // Return hash as a positive hexadecimal string
+};
+
+const loadScriptWithFallback = (
+  urls: string[],
+  getEntry: () => OidcModule,
+): Promise<OidcModule> => {
+  return new Promise((resolve, reject) => {
+    if (!urls.length)
+      return reject(new Error('No URLs provided to loadScriptWithFallback'));
+
+    const entry = getEntry();
+    if (entry) return resolve(entry);
+
+    const url = urls.shift();
+
+    const scriptEle = document.createElement('script');
+    scriptEle.src = url;
+    scriptEle.id = simpleHash(url);
+    scriptEle.onload = () => {
+      const entry = getEntry();
+      if (entry) return resolve(entry);
+      throw new Error('Could not get entry after loading script from URL');
+    };
+    scriptEle.addEventListener('error', () => {
+      loadScriptWithFallback(urls, getEntry);
+      scriptEle.setAttribute('data-error', 'true');
+    });
+    document.body.appendChild(scriptEle);
+  });
+};
+
+const loadOIDCModule = async (): Promise<OidcModule> => {
+  try {
+    return import('oidc-client-ts');
+  } catch (e) {
+    return loadScriptWithFallback(
+      [OIDC_CLIENT_TS_DESCOPE_CDN_URL, OIDC_CLIENT_TS_JSDELIVR_CDN_URL],
+      () => window['oidc'],
+    );
+  }
+};
+
+const getOidcClient = async (
   sdk: CoreSdk,
   projectID: string,
   oidcConfig?: OidcConfig,
 ) => {
-  // oidc-client-ts is a peer dependency of @descope/web-js-sdk
-  // so if it's not installed, we should throw an error
+  if (!scriptLoadingPromise) {
+    scriptLoadingPromise = loadOIDCModule();
+  }
+  const { OidcClient, WebStorageStateStore } = await scriptLoadingPromise;
+
   if (!OidcClient) {
     throw new Error(
       'oidc-client-ts is not installed. Please install it by running `npm install oidc-client-ts`',
@@ -59,7 +125,7 @@ const createOidc = (sdk: CoreSdk, projectID: string) => {
   const authorize = async (
     oidcConfig?: OidcConfig,
   ): Promise<SdkResponse<URLResponse>> => {
-    const { client } = getOidcClient(sdk, projectID, oidcConfig);
+    const { client } = await getOidcClient(sdk, projectID, oidcConfig);
     const { url } = await client.createSigninRequest({});
     return { ok: true, data: { url } };
   };
@@ -67,7 +133,11 @@ const createOidc = (sdk: CoreSdk, projectID: string) => {
   const token = async (
     oidcConfig?: OidcConfig,
   ): Promise<SdkResponse<JWTResponse>> => {
-    const { client, stateUserKey } = getOidcClient(sdk, projectID, oidcConfig);
+    const { client, stateUserKey } = await getOidcClient(
+      sdk,
+      projectID,
+      oidcConfig,
+    );
     const user = await client.processSigninResponse(window.location.href);
     window.localStorage.setItem(stateUserKey, JSON.stringify(user));
     return sdk.refresh(user.refresh_token);
@@ -79,7 +149,11 @@ const createOidc = (sdk: CoreSdk, projectID: string) => {
   };
 
   const logout = async (oidcConfig?: OidcConfig) => {
-    const { client, stateUserKey } = getOidcClient(sdk, projectID, oidcConfig);
+    const { client, stateUserKey } = await getOidcClient(
+      sdk,
+      projectID,
+      oidcConfig,
+    );
     const user = getUser(stateUserKey);
     if (user) {
       const { url } = await client.createSignoutRequest({
