8b1869853a78e347ff258fd733483a2f64716719
Tomer Lichtash <tomer@descope.com>
2024-12-23 13:27:41 +0200

    remove change

--- packages/sdks/react-sdk/src/constants.ts
+++ packages/sdks/react-sdk/src/constants.ts
@@ -5,5 +5,5 @@ export const baseHeaders = {
   'x-descope-sdk-version': BUILD_VERSION,
 };
 
-// This sdk can be used in SSR apps.
+// This sdk can be used in SSR apps
 export const IS_BROWSER = typeof window !== 'undefined';
