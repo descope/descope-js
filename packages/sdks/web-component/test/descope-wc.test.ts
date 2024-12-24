f3e437e0793507627b157317063fe39174600c80
nirgur <nirgur@users.noreply.github.com>
2024-12-24 14:44:28 +0200

    feat: Content from base url (#871) RELEASE
    ## Related Issues
    
    Fixes https://github.com/descope/etc/issues/8253

--- packages/sdks/web-component/test/descope-wc.test.ts
+++ packages/sdks/web-component/test/descope-wc.test.ts
@@ -3009,7 +3009,7 @@ describe('web-component', () => {
       await waitFor(() => expect(startMock).toHaveBeenCalled());
 
       await waitFor(() => screen.findByShadowText('It works!'), {
-        timeout: WAIT_TIMEOUT,
+        timeout: WAIT_TIMEOUT * 2,
       });
 
       fireEvent.click(screen.getByShadowText('click'));
