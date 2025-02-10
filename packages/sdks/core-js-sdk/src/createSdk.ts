d8db39a4dfe0b109dc4b63e87396bf96bace9e94
Omer C <639682+omercnet@users.noreply.github.com>
2025-02-10 21:24:26 +0200

    fix

--- packages/sdks/core-js-sdk/src/createSdk.ts
+++ packages/sdks/core-js-sdk/src/createSdk.ts
@@ -10,8 +10,6 @@ type SdkConfig = {
   projectId: string;
   logger?: Logger;
   baseUrl?: string;
-  baseStaticUrl?: string;
-  baseCdnUrl?: string;
   hooks?: Hooks;
   cookiePolicy?: RequestCredentials | null;
   baseHeaders?: HeadersInit;
@@ -26,47 +24,47 @@ const withSdkConfigValidations = withValidations([
 /** Add the ability to pass multiple hooks instead of one when creating an SDK instance */
 const withMultipleHooks =
   <T extends object>(createSdk: (config: SdkConfig) => T) =>
-  (
-    config: Omit<SdkConfig, 'hooks'> & {
-      hooks?: {
-        beforeRequest?: BeforeRequest | BeforeRequest[];
-        afterRequest?: AfterRequest | AfterRequest[];
-        transformResponse?: Hooks['transformResponse'];
-      };
-    },
-  ) => {
-    const beforeRequest: BeforeRequest = (conf) => {
-      // get the before hooks from the config while function is running
-      // because the hooks might change after sdk creation
-      const beforeRequestHooks = [].concat(config.hooks?.beforeRequest || []);
-      return beforeRequestHooks?.reduce((acc, fn) => fn(acc), conf);
-    };
-
-    const afterRequest: AfterRequest = async (req, res) => {
-      // get the after hooks from the config while function is running
-      // because the hooks might change after sdk creation
-      const afterRequestHooks = [].concat(config.hooks?.afterRequest || []);
-      // do not remove this check - on old versions of react-native it is required
-      if (afterRequestHooks.length == 0) return;
-      const results = await Promise.allSettled(
-        afterRequestHooks?.map((fn) => fn(req, res?.clone())),
-      );
-      // eslint-disable-next-line no-console
-      results.forEach(
-        (result) =>
-          result.status === 'rejected' && config.logger?.error(result.reason),
-      );
-    };
-
-    return createSdk({
-      ...config,
-      hooks: {
-        beforeRequest,
-        afterRequest,
-        transformResponse: config.hooks?.transformResponse,
+    (
+      config: Omit<SdkConfig, 'hooks'> & {
+        hooks?: {
+          beforeRequest?: BeforeRequest | BeforeRequest[];
+          afterRequest?: AfterRequest | AfterRequest[];
+          transformResponse?: Hooks['transformResponse'];
+        };
       },
-    });
-  };
+    ) => {
+      const beforeRequest: BeforeRequest = (conf) => {
+        // get the before hooks from the config while function is running
+        // because the hooks might change after sdk creation
+        const beforeRequestHooks = [].concat(config.hooks?.beforeRequest || []);
+        return beforeRequestHooks?.reduce((acc, fn) => fn(acc), conf);
+      };
+
+      const afterRequest: AfterRequest = async (req, res) => {
+        // get the after hooks from the config while function is running
+        // because the hooks might change after sdk creation
+        const afterRequestHooks = [].concat(config.hooks?.afterRequest || []);
+        // do not remove this check - on old versions of react-native it is required
+        if (afterRequestHooks.length == 0) return;
+        const results = await Promise.allSettled(
+          afterRequestHooks?.map((fn) => fn(req, res?.clone())),
+        );
+        // eslint-disable-next-line no-console
+        results.forEach(
+          (result) =>
+            result.status === 'rejected' && config.logger?.error(result.reason),
+        );
+      };
+
+      return createSdk({
+        ...config,
+        hooks: {
+          beforeRequest,
+          afterRequest,
+          transformResponse: config.hooks?.transformResponse,
+        },
+      });
+    };
 
 /** Descope SDK client */
 export default withSdkConfigValidations(
