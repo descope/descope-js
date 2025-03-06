7e9e49ddd33134034ba19f2eaa87cab7a2b67dab
Nir Gur Arie <nir@descope.com>
2025-03-06 21:05:48 +0200

    add totp to UPW

---
+++ packages/widgets/user-profile-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initTotpUserAuthMethodMixin.ts
@@ -0,0 +1,99 @@
+import {
+  FlowDriver,
+  ModalDriver,
+  UserAuthMethodDriver,
+} from '@descope/sdk-component-drivers';
+import {
+  compose,
+  createSingletonMixin,
+  withMemCache,
+} from '@descope/sdk-helpers';
+import {
+  cookieConfigMixin,
+  loggerMixin,
+  modalMixin,
+} from '@descope/sdk-mixins';
+import { stateManagementMixin } from '../../stateManagementMixin';
+import { initWidgetRootMixin } from './initWidgetRootMixin';
+import { getHasTotp } from '../../../state/selectors';
+import { createFlowTemplate } from '../../helpers';
+import { flowSyncThemeMixin } from '../../flowSyncThemeMixin';
+
+export const initTotpUserAuthMethodMixin = createSingletonMixin(
+  <T extends CustomElementConstructor>(superclass: T) =>
+    class TotpUserAuthMethodMixinClass extends compose(
+      flowSyncThemeMixin,
+      stateManagementMixin,
+      loggerMixin,
+      initWidgetRootMixin,
+      cookieConfigMixin,
+      modalMixin,
+    )(superclass) {
+      totpUserAuthMethod: UserAuthMethodDriver;
+
+      #modal: ModalDriver;
+
+      #flow: FlowDriver;
+
+      #initModal() {
+        if (!this.totpUserAuthMethod.flowId) return;
+
+        this.#modal = this.createModal({ 'data-id': 'totp' });
+        this.#flow = new FlowDriver(
+          () => this.#modal.ele?.querySelector('descope-wc'),
+          { logger: this.logger },
+        );
+        this.#modal.afterClose = this.#initModalContent.bind(this);
+        this.#initModalContent();
+        this.syncFlowTheme(this.#flow);
+      }
+
+      #initModalContent() {
+        this.#modal.setContent(
+          createFlowTemplate({
+            projectId: this.projectId,
+            flowId: this.totpUserAuthMethod.flowId,
+            baseUrl: this.baseUrl,
+            baseStaticUrl: this.baseStaticUrl,
+            baseCdnUrl: this.baseCdnUrl,
+            refreshCookieName: this.refreshCookieName,
+          }),
+        );
+        this.#flow.onSuccess(() => {
+          this.#modal.close();
+          this.actions.getMe();
+        });
+      }
+
+      #initTotpAuthMethod() {
+        this.totpUserAuthMethod = new UserAuthMethodDriver(
+          () =>
+            this.shadowRoot?.querySelector(
+              'descope-user-auth-method[data-id="totp"]',
+            ),
+          { logger: this.logger },
+        );
+
+        this.totpUserAuthMethod.onButtonClick(() => {
+          this.#modal?.open();
+        });
+      }
+
+      #onFulfilledUpdate = withMemCache(
+        (hasTotp: ReturnType<typeof getHasTotp>) => {
+          this.totpUserAuthMethod.fulfilled = hasTotp;
+        },
+      );
+
+      async onWidgetRootReady() {
+        await super.onWidgetRootReady?.();
+
+        this.#initTotpAuthMethod();
+        this.#initModal();
+
+        this.#onFulfilledUpdate(getHasTotp(this.state));
+
+        this.subscribe(this.#onFulfilledUpdate.bind(this), getHasTotp);
+      }
+    },
+);
