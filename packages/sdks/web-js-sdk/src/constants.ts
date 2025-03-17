4e523574f5ad44d4a2204d5b2e62ba001abe62fe
Asaf Shen <asaf@descope.com>
2025-03-17 18:39:37 +0700

    fix: short timeout (#1048) RELEASE
    ## Related Issues
    
    Fixes https://github.com/descope/etc/issues/8912
    Fixes https://github.com/descope/etc/issues/7424
    
    ## Description
    
    handle too close timeouts in the following ways
    - after auth request - if calculated timeout is less than 20 seconds -
    do not set refresh timeout
     - when window becomes visible - do not refresh if refresh failed

--- packages/sdks/web-js-sdk/src/constants.ts
+++ packages/sdks/web-js-sdk/src/constants.ts
@@ -4,3 +4,6 @@ export const IS_BROWSER = typeof window !== 'undefined';
 // Maximum timeout value for setTimeout
 // For more information, refer to https://developer.mozilla.org/en-US/docs/Web/API/setTimeout#maximum_delay_value
 export const MAX_TIMEOUT = Math.pow(2, 31) - 1;
+
+// The amount of time (ms) to trigger the refresh before session expires
+export const REFRESH_THRESHOLD = 20 * 1000; // 20 sec
