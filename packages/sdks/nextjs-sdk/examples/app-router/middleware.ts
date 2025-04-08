184b7211d9bf6f1571832b3c377451a68d0edc42
Asaf Shen <asaf@descope.com>
2025-04-08 16:34:32 +0300

    remove redundant log

--- packages/sdks/nextjs-sdk/examples/app-router/middleware.ts
+++ packages/sdks/nextjs-sdk/examples/app-router/middleware.ts
@@ -1,9 +1,5 @@
 import { authMiddleware } from '@descope/nextjs-sdk/server';
 
-console.log('@@@ middleware', {
-	projectId: process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID,
-	baseUrl: process.env.NEXT_PUBLIC_DESCOPE_BASE_URL
-});
 export default authMiddleware({
 	projectId: process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID,
 	baseUrl: process.env.NEXT_PUBLIC_DESCOPE_BASE_URL,
