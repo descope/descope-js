a60321e39362566c8ffa4589c9b5eb83b574ef0f
Tomer Lichtash <tomer@descope.com>
2025-07-02 17:29:34 +0300

    WIP

---
+++ packages/widgets/user-profile-widget/src/lib/widget/mixins/initMixin/initComponentsMixins/initRecoveryCodesUserAuthMethodMixin.ts
@@ -0,0 +1,100 @@
+import {
+  FlowDriver,
+  ModalDriver,
+  UserAuthMethodDriver,
+} from '@descope/sdk-component-drivers';
+import {
+  compose,
+  createSingletonMixin,
+  withMemCache,
+} from '@descope/sdk-helpers';
+import {
+  cookieConfigMixin,
+  loggerMixin,
+  modalMixin,
+} from '@descope/sdk-mixins';
+import { stateManagementMixin } from '../../stateManagementMixin';
+import { initWidgetRootMixin } from './initWidgetRootMixin';
+import { getHasRecoveryCodes } from '../../../state/selectors';
+import { createFlowTemplate } from '../../helpers';
+import { flowSyncThemeMixin } from '../../flowSyncThemeMixin';
+
+export const initRecoveryCodesUserAuthMethodMixin = createSingletonMixin(
+  <T extends CustomElementConstructor>(superclass: T) =>
+    class RecoveryCodesUserAuthMethodMixinClass extends compose(
+      flowSyncThemeMixin,
+      stateManagementMixin,
+      loggerMixin,
+      initWidgetRootMixin,
+      cookieConfigMixin,
+      modalMixin,
+    )(superclass) {
+      recoveryCodesUserAuthMethod: UserAuthMethodDriver;
+
+      #modal: ModalDriver;
+
+      #flow: FlowDriver;
+
+      #initModal() {
+        if (!this.recoveryCodesUserAuthMethod.flowId) return;
+
+        this.#modal = this.createModal({ 'data-id': 'recovery-codes' });
+        this.#flow = new FlowDriver(
+          () => this.#modal.ele?.querySelector('descope-wc'),
+          { logger: this.logger },
+        );
+        this.#modal.afterClose = this.#initModalContent.bind(this);
+        this.#initModalContent();
+        this.syncFlowTheme(this.#flow);
+      }
+
+      #initModalContent() {
+        this.#modal.setContent(
+          createFlowTemplate({
+            projectId: this.projectId,
+            flowId: this.recoveryCodesUserAuthMethod.flowId,
+            baseUrl: this.baseUrl,
+            baseStaticUrl: this.baseStaticUrl,
+            baseCdnUrl: this.baseCdnUrl,
+            refreshCookieName: this.refreshCookieName,
+            theme: this.theme,
+          }),
+        );
+        this.#flow.onSuccess(() => {
+          this.#modal.close();
+          this.actions.getMe();
+        });
+      }
+
+      #initRecoveryCodesAuthMethod() {
+        this.recoveryCodesUserAuthMethod = new UserAuthMethodDriver(
+          () =>
+            this.shadowRoot?.querySelector(
+              'descope-user-auth-method[data-id="recovery-codes"]',
+            ),
+          { logger: this.logger },
+        );
+
+        this.recoveryCodesUserAuthMethod.onButtonClick(() => {
+          this.#modal?.open();
+        });
+      }
+
+      #onFulfilledUpdate = withMemCache(
+        (hasRecoveryCodes: ReturnType<typeof getHasRecoveryCodes>) => {
+          this.recoveryCodesUserAuthMethod.fulfilled = hasRecoveryCodes;
+        },
+      );
+
+      async onWidgetRootReady() {
+        await super.onWidgetRootReady?.();
+
+        this.#initRecoveryCodesAuthMethod();
+        this.#initModal();
+
+        this.#onFulfilledUpdate(getHasRecoveryCodes(this.state));
+
+        this.subscribe(this.#onFulfilledUpdate.bind(this), getHasRecoveryCodes);
+      }
+    },
+);
