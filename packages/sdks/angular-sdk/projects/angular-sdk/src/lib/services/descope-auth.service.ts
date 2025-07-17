1ebd85ba14f7558e32876d7f2964bf08ee8c93aa
Asaf Shen <asaf@descope.com>
2025-07-17 13:06:45 +0300

    feat: add auto refresh config to web-framework sdks (#1149)
    ## Related Issues
    
    Fixes https://github.com/descope/etc/issues/11249
    
    
    ## Description
    add auto refresh config to web-framework sdks
     - React (this includes support to NextJS)
     - Vue
     - Angular

--- packages/sdks/angular-sdk/projects/angular-sdk/src/lib/services/descope-auth.service.ts
+++ packages/sdks/angular-sdk/projects/angular-sdk/src/lib/services/descope-auth.service.ts
@@ -39,9 +39,9 @@ export class DescopeAuthService {
     this.descopeSdk = observabilify<DescopeSDK>(
       createSdk({
         persistTokens: isBrowser() as true,
-        ...config,
         storeLastAuthenticatedUser: isBrowser() as true,
         autoRefresh: isBrowser() as true,
+        ...config,
         baseHeaders
       })
     );
