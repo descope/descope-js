d379047832a94287c4bbfb6d096c27a3e1051a1a
Nitzan Peretz <nitzan@descope.com>
2025-07-22 17:00:40 +0300

    feat: Tenant admin widget (#1158)
    ## Related Issues
    
    Part of https://github.com/descope/etc/issues/5487
    
    ## Related PRs
    
    Console-app
    Management
    Content
    
    ## Description
    
    New Tenant Profile Widget.
    
    ## Must
    
    - [x] Tests
    - [x] Documentation (if applicable)
    
    ---------
    
    Signed-off-by: Nitzan Peretz <nitzan@descope.com>
    Co-authored-by: Bar Saar <bar@descope.com>
    Co-authored-by: nirgur <nirgur@users.noreply.github.com>

---
+++ packages/widgets/tenant-profile-widget/src/lib/widget/state/selectors.ts
@@ -0,0 +1,33 @@
+import { createSelector } from 'reselect';
+import { State } from './types';
+
+export const getMe = (state: State) => state.me.data;
+export const getMeError = (state: State) => state.me.error;
+
+export const getTenant = (state: State) => state.tenant.data;
+export const getTenantError = (state: State) => state.tenant.error;
+
+export const getTenantAdminLinkSSO = (state: State) =>
+  state.tenantAdminLinkSSO.data;
+export const getTenantAdminLinkSSOError = (state: State) =>
+  state.tenantAdminLinkSSO.error;
+
+export const getTenantCustomAttributes = createSelector(
+  getTenant,
+  (tenant) => tenant.customAttributes || ({} as Record<string, any>),
+);
+
+export const getTenantName = createSelector(
+  getTenant,
+  (tenant) => tenant.name || '',
+);
+
+export const getTenantEmailDomains = createSelector(
+  getTenant,
+  (tenant) => tenant.selfProvisioningDomains || [],
+);
+
+export const getTenantEnforceSSO = createSelector(
+  getTenant,
+  (tenant) => tenant.enforceSSO || false,
+);
