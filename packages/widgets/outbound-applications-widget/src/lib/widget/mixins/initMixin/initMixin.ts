022bd96d25d3e9e30e573eb87e7df2332563ee3b
Tomer Lichtash <tomer@descope.com>
2025-07-30 14:25:31 +0300

    fix sdk load and cleanup

--- packages/widgets/outbound-applications-widget/src/lib/widget/mixins/initMixin/initMixin.ts
+++ packages/widgets/outbound-applications-widget/src/lib/widget/mixins/initMixin/initMixin.ts
@@ -2,7 +2,6 @@ import { compose, createSingletonMixin } from '@descope/sdk-helpers';
 import { debuggerMixin, themeMixin } from '@descope/sdk-mixins';
 import { flowRedirectUrlMixin } from '../flowRedirectUrlMixin';
 import { initOutboundAppsListMixin } from './initComponentsMixins/initOutboundAppsListMixin';
-import { customAppsMixin } from './customAppsMixin';
 
 export const initMixin = createSingletonMixin(
   <T extends CustomElementConstructor>(superclass: T) =>
@@ -11,7 +10,6 @@ export const initMixin = createSingletonMixin(
       debuggerMixin,
       themeMixin,
       flowRedirectUrlMixin, // This mixin must be before all other mixins that loads flows
-      customAppsMixin,
       initOutboundAppsListMixin,
     )(superclass) {
       async init() {
