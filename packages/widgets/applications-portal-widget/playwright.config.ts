595284b01b761a8f4cf31b04291b15da671b1126
Tomer Lichtash <tomer@descope.com>
2025-08-04 17:12:09 +0300

    Use port-generator in widget playwright specs

--- packages/widgets/applications-portal-widget/playwright.config.ts
+++ packages/widgets/applications-portal-widget/playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import { getWidgetTestPorts } from '@descope/e2e-helpers';
+
+export const [componentsPort, widgetPort] = getWidgetTestPorts();
 
 /**
  * Read environment variables from file.
@@ -26,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5560',
+    baseURL: `http://localhost:${widgetPort}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -89,11 +92,11 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: 'npx serve node_modules/@descope/web-components-ui/dist -p 8769',
+      command: `npx serve node_modules/@descope/web-components-ui/dist -p ${componentsPort}`,
     },
     {
-      command: 'npx serve build -l 5560',
-      url: 'http://localhost:5560',
+      command: `npx serve build -l ${widgetPort}`,
+      url: `http://localhost:${widgetPort}`,
       reuseExistingServer: !process.env.CI,
       timeout: 120 * 1000,
     },
