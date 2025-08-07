251fff6b6caac9d67c9ab6c309708a58077b8ed4
nirgur <nirgur@users.noreply.github.com>
2025-08-07 17:57:58 +0300

    fix: added BYOS example & fix Angular 19 issues

--- packages/sdks/angular-sdk/projects/demo-app/src/app/my-applications-portal/my-applications-portal.component.ts
+++ packages/sdks/angular-sdk/projects/demo-app/src/app/my-applications-portal/my-applications-portal.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
+import { DescopeAuthModule } from '../../../../angular-sdk/src/lib/descope-auth.module';
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-my-applications-portal',
   templateUrl: './my-applications-portal.component.html',
-  styleUrls: ['./my-applications-portal.scss']
+  styleUrls: ['./my-applications-portal.scss'],
+  standalone: true,
+  imports: [DescopeAuthModule]
 })
 export class MyApplicationsPortalComponent {
   theme = (environment.descopeTheme as 'light' | 'dark' | 'os') ?? 'os';
