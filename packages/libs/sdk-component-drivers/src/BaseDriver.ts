726f4cba46154e50bc1296d21acf5ba8a803da74
nirgur <nirgur@users.noreply.github.com>
2025-08-13 19:42:06 +0300

    added support for multiple elements selector in base driver

--- packages/libs/sdk-component-drivers/src/BaseDriver.ts
+++ packages/libs/sdk-component-drivers/src/BaseDriver.ts
@@ -1,5 +1,5 @@
-import { waitForElement } from './helpers';
-import { RefOrRefFn } from './types';
+import { waitFor } from './helpers';
+import { DriverElements, DriverElement, Empty, ElementsProxy } from './types';
 
 type Logger = {
   error(...data: any[]): void;
@@ -8,25 +8,26 @@ type Logger = {
   debug(...data: any[]): void;
 };
 
-export class BaseDriver {
-  #ele: RefOrRefFn;
+export class BaseDriver<
+  T extends DriverElement | DriverElements = DriverElement,
+> {
+  #ele: T;
 
   logger: Logger | undefined;
 
   // eslint-disable-next-line class-methods-use-this
   nodeName = '';
 
-  constructor(refOrRefFn: RefOrRefFn, config: { logger: Logger }) {
+  constructor(refOrRefFn: T, config: { logger: Logger }) {
     this.#ele = refOrRefFn;
     this.logger = config.logger;
   }
 
   get asyncEle() {
-    return waitForElement(this.#ele, 1000);
+    return waitFor(() => this.ele, 1000);
   }
 
-  get ele() {
-    const ele = typeof this.#ele === 'function' ? this.#ele() : this.#ele;
+  #handleSingleElement(ele: Element | Empty) {
     if (!ele) {
       this.logger?.debug(
         `Driver element is not available for ${this.nodeName}`,
@@ -46,5 +47,87 @@ export class BaseDriver {
     }
 
     return ele;
+  }
+
+  handleMultiElements(ele: NodeListOf<Element> | Empty): ElementsProxy | null {
+    if (!ele || ele.length === 0) {
+      this.logger?.debug(
+        `Driver elements are not available for ${this.nodeName}`,
+        new Error(),
+      );
+      return null;
+    }
+
+    const eles = Array.from(ele);
+
+    if (eles.some((e) => e.localName !== this.nodeName)) {
+      this.logger?.debug(
+        `node name do not match, expected "${this.nodeName}", received "${eles
+          .map((e) => e.localName)
+          .join(', ')}" `,
+        Error(),
+      );
+
+      return null;
+    }
+
+    const createProxy = (elements: Element[]): ElementsProxy => {
+      return new Proxy(
+        {},
+        {
+          get(_, prop, receiver) {
+            if (prop === Symbol.iterator) {
+              // Allow: for (const el of $items) { ... }
+              return function* () {
+                yield* elements;
+              };
+            }
+            if (prop === 'length') return elements.length;
+
+            if (prop === 'filter') {
+              return (cb: (el: Element) => boolean) => {
+                const filteredEles = elements.filter(cb);
+                return createProxy(filteredEles);
+              };
+            }
+
+            if (elements.every((el) => typeof el[prop] === 'function')) {
+              return (...args) =>
+                elements.map((el) => el[prop].apply(el, args));
+            }
+
+            return elements.map((el) => el[prop]);
+          },
+
+          set(_, prop, val) {
+            elements.forEach((el) => {
+              el[prop] = val;
+            });
+            return true;
+          },
+
+          has(_, prop) {
+            return elements.every((e) => prop in e);
+          },
+        },
+      ) as ElementsProxy;
+    };
+
+    return createProxy(eles);
+  }
+
+  get ele(): T extends DriverElement ? Element | null : ElementsProxy | null {
+    const ele = typeof this.#ele === 'function' ? this.#ele() : this.#ele;
+
+    if (ele instanceof Element) {
+      return this.#handleSingleElement(ele) as T extends DriverElement
+        ? Element | null
+        : ElementsProxy | null;
+    } else if (ele instanceof NodeList) {
+      return this.handleMultiElements(ele) as T extends DriverElement
+        ? Element | null
+        : ElementsProxy | null;
+    }
+    return null;
   }
 }
--- packages/libs/sdk-component-drivers/src/types.ts
+++ packages/libs/sdk-component-drivers/src/types.ts
@@ -1,3 +1,22 @@
-type Empty = null | undefined;
+export type Empty = null | undefined;
 
-export type RefOrRefFn = Element | (() => HTMLElement | Empty) | Empty;
+export type DriverEle = Element | Empty;
+type DriverEleGetter = () => DriverEle;
+
+export type DriverEles = NodeListOf<Element>;
+type DriverElesGetter = () => DriverEles;
+
+export type DriverElement = DriverEle | DriverEleGetter;
+export type DriverElements = DriverEles | DriverElesGetter;
+
+// Type for the proxy returned by handleNodeList
+export type ElementsProxy = {
+  readonly length: number;
+  [Symbol.iterator](): IterableIterator<Element>;
+  filter(callback: (element: Element) => boolean): ElementsProxy;
+} & {
+  // All properties that exist on Element, but as arrays of those properties
+  [K in keyof Element]: Element[K] extends (...args: any[]) => any
+    ? (...args: Parameters<Element[K]>) => ReturnType<Element[K]>[]
+    : Element[K][];
+};
