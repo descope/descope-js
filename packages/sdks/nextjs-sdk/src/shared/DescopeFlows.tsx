cfc9883cd834d5a24cc17ef8bb9a3d544ea2328f
nirgur <nirgur@users.noreply.github.com>
2025-10-09 13:15:04 +0300

    CR fixes

--- packages/sdks/nextjs-sdk/src/shared/DescopeFlows.tsx
+++ packages/sdks/nextjs-sdk/src/shared/DescopeFlows.tsx
@@ -18,6 +18,8 @@ interface RedirectAddon {
 	redirectAfterError?: string;
 }
 
+type WithRedirect<T> = T & RedirectAddon;
+
 export type BaseDescopeProps = React.ComponentProps<typeof DescopeWC>;
 export type BaseSignInFlowProps = React.ComponentProps<typeof SignInFlowWC>;
 export type BaseSignUpFlowProps = React.ComponentProps<typeof SignUpFlowWC>;
@@ -50,25 +52,30 @@ function createDescopeWrapper<T extends AnyFlowProps>(
 			import('@descope/react-sdk').then((mod: any) => {
 				if (mod.baseHeaders) Object.assign(mod.baseHeaders, nextBaseHeaders);
 				const Inner = mod[componentName] as React.ComponentType<any>;
-				const Wrapped = ({
+				const Wrapped: React.FC<WithRedirect<T>> = ({
 					redirectAfterSuccess = '',
 					redirectAfterError = '',
 					...rest
-				}: any) => {
+				}) => {
 					const router = useRouter();
-					const forwarded: any = { ...rest };
+					// we purposefully allow index access because we don't know exact handler names ahead; use a mutable copy
+					const forwarded = { ...rest } as T;
 					if (redirectAfterSuccess) {
-						const original = forwarded.onSuccess;
+						const original = forwarded.onSuccess as
+							| ((...a: any[]) => void)
+							| undefined;
 						forwarded.onSuccess = (...args: any[]) => {
 							original?.(...args);
-							router.push(redirectAfterSuccess as string);
+							router.push(redirectAfterSuccess);
 						};
 					}
 					if (redirectAfterError) {
-						const original = forwarded.onError;
+						const original = forwarded.onError as
+							| ((...a: any[]) => void)
+							| undefined;
 						forwarded.onError = (...args: any[]) => {
 							original?.(...args);
-							router.push(redirectAfterError as string);
+							router.push(redirectAfterError);
 						};
 					}
 					return React.createElement(Inner, forwarded);
