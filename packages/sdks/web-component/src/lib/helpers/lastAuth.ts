8b53dd3b5a0aec2db60bef9e0c89ebb8f4071fd9
aviadl <aviad@descope.com>
2025-10-15 22:20:58 +0300

    Store last login id if exists with last auth fixes https://github.com/descope/etc/issues/11549

--- packages/sdks/web-component/src/lib/helpers/lastAuth.ts
+++ packages/sdks/web-component/src/lib/helpers/lastAuth.ts
@@ -1,6 +1,7 @@
 import { DESCOPE_LAST_AUTH_LOCAL_STORAGE_KEY } from '../constants';
 import { getStorageItem, setStorageItem } from './storage';
 import { NextFnReturnPromiseValue } from '../types';
+import { setLastUserLoginId } from '@descope/web-js-sdk';
 
 export function getLastAuth(loginId: string) {
   const lastAuth = {};
@@ -31,4 +32,7 @@ export function setLastAuth(
     return;
   }
   setStorageItem(DESCOPE_LAST_AUTH_LOCAL_STORAGE_KEY, JSON.stringify(lastAuth));
+  if ((lastAuth as any)?.loginId) {
+    setLastUserLoginId((lastAuth as any)?.loginId);
+  }
 }
