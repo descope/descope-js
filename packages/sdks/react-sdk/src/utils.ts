b6b04f47f04e3bb9f14a6d49f84b805993f4e637
Gil Shapira <gshapira@gmail.com>
2025-11-27 15:15:16 +0200

    Skip session handling when running in native flows

--- packages/sdks/react-sdk/src/utils.ts
+++ packages/sdks/react-sdk/src/utils.ts
@@ -25,3 +25,6 @@ export const wrapInTry =
     }
     return res;
   };
+
+// Detect if running in a native flow (e.g., mobile app with Descope bridge in a webview)
+export const isDescopeBridge = () => !!(window as any)?.descopeBridge;
--- packages/sdks/web-js-sdk/src/enhancers/helpers/index.ts
+++ packages/sdks/web-js-sdk/src/enhancers/helpers/index.ts
@@ -144,9 +144,8 @@ export const getUserFromResponse = async (
   );
 };
 
-// This window flag is set by mobile frameworks
-export const isDescopeBridge = () =>
-  typeof window !== 'undefined' && !!window['descopeBridge'];
+// Detect if running in a native flow (e.g., mobile app with Descope bridge in a webview)
+export const isDescopeBridge = () => !!(window as any)?.descopeBridge;
 
 export const isLocalStorage =
   typeof customStorage !== 'undefined' || typeof localStorage !== 'undefined';
