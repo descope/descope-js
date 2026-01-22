bc9a41ca55a82ef7ec73e6c2d05f54969aacb315
Tomer Lichtash <tomer@descope.com>
2026-01-22 13:16:56 +0200

    cleanup

--- packages/widgets/user-profile-widget/src/lib/widget/api/sdk/createTenantSdk.ts
+++ packages/widgets/user-profile-widget/src/lib/widget/api/sdk/createTenantSdk.ts
@@ -2,13 +2,7 @@ import { apiPaths } from '../apiPaths';
 import { HttpClient } from '../types';
 import { withErrorHandler } from './helpers';
 
-export const createTenantSdk = ({
-  httpClient,
-  mock,
-}: {
-  httpClient: HttpClient;
-  mock: boolean;
-}) => {
+export const createTenantSdk = ({ httpClient }: { httpClient: HttpClient }) => {
   const selectTenant = async (tenantId: string) => {
     const res = await httpClient.post(apiPaths.user.selectTenant, {
       tenant: tenantId,
