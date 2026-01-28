9ff4504b75f8faf169ee2d925698f98a5cc80aa5
Tomer Lichtash <tomer@descope.com>
2026-01-28 17:36:39 +0200

    change tenant selector api to data attrs

--- packages/libs/sdk-component-drivers/src/TenantSelectorDriver.ts
+++ packages/libs/sdk-component-drivers/src/TenantSelectorDriver.ts
@@ -2,11 +2,14 @@ import { SingleSelectDriver } from './SingleSelectDriver';
 
 export class TenantSelectorDriver extends SingleSelectDriver {
   get action(): 'reload' | 'none' | '' {
-    return (this.ele?.getAttribute('action') || '') as 'reload' | 'none' | '';
+    return (this.ele?.getAttribute('data-action') || '') as
+      | 'reload'
+      | 'none'
+      | '';
   }
 
   get refreshTimeout(): number {
-    return Number(this.ele?.getAttribute('refresh-timeout')) || 500;
+    return Number(this.ele?.getAttribute('data-refresh-timeout')) || 500;
   }
 
   get shouldReload() {
